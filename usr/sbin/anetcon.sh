cba3af5d0fd7ab03f57aaafc5d4422008e54c968
Carlos A. <caralla@upv.es>
2024-06-27 12:37:59 +0200

    add a hash for the name of the namespaces and custom log prefixes

--- usr/sbin/anetcon.sh
+++ usr/sbin/anetcon.sh
@@ -16,14 +16,30 @@ NETS=( )
 LOGFILE=/var/log/anetcon/anetcon.log
 # Chain in which to join anetcon (POSTROUTING should be fine in most cases)
 CHAIN_TO_JOIN=POSTROUTING
-
 # The content of the iptables from the router that is being analized
 IPTABLES_NAMESPACE=( )
 # The networks that should be monitorized for the router that is being analized
 EXPECTED_NETS=( )
-# The current router that is being analized
-ROUTER_ID=
-ROUTER_NS=
+# The current namespace
+CURRENT_NS=
+CURRENT_NS_LOGHASH=
+# The name of the chain for anetcon
+CHAIN_NAME=anetcon
+# Prefix for the log messages
+LOG_PREFIX="[ANETCON]"
+LOG_PREFIX_DNAT="[D]"
+LOG_PREFIX_OTHER="[X]"
+# Whether to include a hash of the namespace in the log messages so each namespace can be identified
+#  (*) true: include the hash; <any other value>: do not include the hash
+LOG_NAMESPACE_HASH=true
+
+# Calculate the hash of a string with a given number of digits
+function hash {
+        local INPUT="$1"
+        local DIGITS="${2:-8}"
+
+        echo -n "$INPUT" | md5sum | cut -d " " -f 1 | cut -c 1-$DIGITS
+}
 
 # Some utility functions
 function p_info {
@@ -65,6 +81,7 @@ function p_error_in {
         [ "$LOGFILE" != "" ] && echo "$TS - [error] - $OUTPUT" >> "$LOGFILE"
 }
 
+# Load the configuration file (if exists)
 if [ -e /etc/anetcon.conf ]; then
         . /etc/anetcon.conf
         if [ $? -ne 0 ]; then
@@ -72,6 +89,7 @@ if [ -e /etc/anetcon.conf ]; then
         fi
 fi
 
+# Wrapper to iptables that allows to execute the command in the current namespace (if any)
 function _iptables {
         if [ "$CURRENT_NS" == "" ]; then
                 iptables -t nat "$@"
@@ -93,26 +111,35 @@ function load_rules {
 
         if [ "$NAMESPACE" == "default" ]; then
                 CURRENT_NS=""
+                CURRENT_NS_LOGHASH=""
         else
                 CURRENT_NS="$NAMESPACE"
+                if [ "$LOG_NAMESPACE_HASH" == "true" ]; then
+                        CURRENT_NS_LOGHASH="[$(hash "$NAMESPACE")]"
+                else
+                        CURRENT_NS_LOGHASH=""
+                fi
         fi
+
+        # Read the iptables rules
         local IPTABLES
         IPTABLES="$(_iptables -S 2> >(p_error_in))"
 
         if [ $? -ne 0 ]; then
                 p_error "failed to load rules for namespace $NAMESPACE"
-
 		IPTABLES_NAMESPACE=( )
                 EXPECTED_NETS=( )
                 return 1
         fi
 
         p_debug "rules for namespace $NAMESPACE successfully loaded"
+
+        # Process the rules to get the ones that are related to anetcon
 	IPTABLES_NAMESPACE=()
 	local RULE
 	while read RULE; do
 		IPTABLES_NAMESPACE+=("$RULE")
-	done <<< "$(echo "$IPTABLES" | grep ' anetcon')"
+	done <<< "$(echo "$IPTABLES" | grep " $CHAIN_NAME")"
         EXPECTED_NETS=( $(get_nets "$NAMESPACE") )
         return 0
 }
@@ -140,12 +167,12 @@ function wipe_rules {
         p_debug "wiping rules for namespace $CURRENT_NS"
 
         # Remove the jumps to our rules
-        while _iptables -D "$CHAIN_TO_JOIN" -j anetcon 2> /dev/null; do
+        while _iptables -D "$CHAIN_TO_JOIN" -j "$CHAIN_NAME" 2> /dev/null; do
                 true
         done
 
-        _iptables -F anetcon
-        _iptables -X anetcon
+        _iptables -F "$CHAIN_NAME"
+        _iptables -X "$CHAIN_NAME"
 }
 
 # Sets the iptables to start logging the start of new connections (this function relies on a previous call to load_rules)
@@ -166,17 +193,17 @@ function create_rules {
                 SOURCE=( "" )
         fi
 
-        _iptables -N anetcon
-        _iptables -I "$CHAIN_TO_JOIN" -j anetcon
+        _iptables -N "$CHAIN_NAME"
+        _iptables -I "$CHAIN_TO_JOIN" -j "$CHAIN_NAME"
 
         # Create the new rules
-        _iptables -A anetcon -p tcp -m tcp -m state --state NEW -m conntrack --ctstate DNAT -j LOG --log-prefix "[ANETCON] [FLOAT] " 2> >(p_error_in)
+        _iptables -A "$CHAIN_NAME" -p tcp -m tcp -m state --state NEW -m conntrack --ctstate DNAT -j LOG --log-prefix "${LOG_PREFIX}${LOG_PREFIX_DNAT}${CURRENT_NS_LOGHASH} " 2> >(p_error_in)
         if [ $? -ne 0 ]; then
                 return 1
         fi
 
         for S in "${SOURCE[@]}"; do
-                _iptables -A anetcon $S -p tcp -m tcp -m state --state NEW  -m conntrack ! --ctstate DNAT  -j LOG --log-prefix "[ANETCON] [NAT] " 2> >(p_error_in)
+                _iptables -A "$CHAIN_NAME" $S -p tcp -m tcp -m state --state NEW  -m conntrack ! --ctstate DNAT  -j LOG --log-prefix "${LOG_PREFIX}${LOG_PREFIX_OTHER}${CURRENT_NS_LOGHASH} " 2> >(p_error_in)
                 if [ $? -ne 0 ]; then
                         return 1
                 fi
@@ -190,16 +217,16 @@ function check_rules {
 
         local NET
         local EXPECTED_RULES=(
-                "-N anetcon"
-                "-A $CHAIN_TO_JOIN -j anetcon"
-                '-A anetcon -p tcp -m tcp -m state --state NEW -m conntrack --ctstate DNAT -j LOG --log-prefix "[ANETCON] [FLOAT] "'
+                "-N $CHAIN_NAME"
+                "-A $CHAIN_TO_JOIN -j $CHAIN_NAME"
+                '-A '"$CHAIN_NAME"' -p tcp -m tcp -m state --state NEW -m conntrack --ctstate DNAT -j LOG --log-prefix "'"${LOG_PREFIX}${LOG_PREFIX_DNAT}${CURRENT_NS_LOGHASH} "'"'
         )
         if ((${#EXPECTED_NETS[@]} > 0)); then
                 for NET in "${EXPECTED_NETS[@]}"; do
-                        EXPECTED_RULES+=('-A anetcon -s $NET -p tcp -m tcp -m state --state NEW -m conntrack ! --ctstate DNAT -j LOG --log-prefix "[ANETCON] [NAT] "')
+                        EXPECTED_RULES+=('-A '"$CHAIN_NAME"' -s $NET -p tcp -m tcp -m state --state NEW -m conntrack ! --ctstate DNAT -j LOG --log-prefix "'"${LOG_PREFIX}${LOG_PREFIX_OTHER}${CURRENT_NS_LOGHASH} "'"')
                 done
         else
-                EXPECTED_RULES+=('-A anetcon -p tcp -m tcp -m state --state NEW -m conntrack ! --ctstate DNAT -j LOG --log-prefix "[ANETCON] [NAT] "')
+                EXPECTED_RULES+=('-A '"$CHAIN_NAME"' -p tcp -m tcp -m state --state NEW -m conntrack ! --ctstate DNAT -j LOG --log-prefix "'"${LOG_PREFIX}${LOG_PREFIX_OTHER}${CURRENT_NS_LOGHASH} "'"')
         fi
 
         p_debug "expected rules for namespace $CURRENT_NS: ${EXPECTED_RULES[@]}"
