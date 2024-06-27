72a48352975adb2ba6503980d893beb55883e647
Carlos A. <caralla@upv.es>
2024-06-27 16:39:12 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+INSTALL_FOLDER=/
+function fixPath() {
+	local RESULT="${1//\/\///}"
+	if [ "$RESULT" != "$1" ]; then
+		fixPath $RESULT
+	else
+		echo $RESULT
+	fi
+}
+
+# Copy the files
+cp etc/logrotate.d/anetcon "$(fixPath "$INSTALL_FOLDER/etc/logrotate.d/")"
+cp etc/rsyslog.d/30-anetcon.conf "$(fixPath "$INSTALL_FOLDER/etc/rsyslog.d/")"
+cp etc/systemd/system/anetcon.service "$(fixPath "$INSTALL_FOLDER/etc/systemd/system/")"
+cp etc/anetcon.conf "$(fixPath "$INSTALL_FOLDER/etc/")"
+cp usr/sbin/anetcon.sh "$(fixPath "$INSTALL_FOLDER/usr/sbin/")"
+mkdir -p "$(fixPath "$INSTALL_FOLDER/var/log/anetcon")"
+mkdir -p "$(fixPath "$INSTALL_FOLDER/var/lib/anetcon")"
+chown syslog:adm "$(fixPath "$INSTALL_FOLDER/var/log/anetcon")"
+chmod +x "$(fixPath "$INSTALL_FOLDER/usr/sbin/anetcon.sh")"
